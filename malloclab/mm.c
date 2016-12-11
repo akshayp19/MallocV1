dafa64c50ceb197027aa3b1d825a2d34b485f5d8
Jason Gross <jsg549@nyu.edu>
2016-12-10 20:46:47 -0500

    Morp.

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -53,13 +53,13 @@ size_t* coalesce(size_t* header){
     size_t* footer = header + (*header)/DSIZE + 1;
     if(*header % DSIZE == 0){
         if (*(header-1) % DSIZE == 0){//If previous block is free
-            printf("a");
+            //printf("a");
             header = header - *(header-1)/DSIZE - 2;//move header to the header of the previous block
             *header += *footer + 2*DSIZE;//Increment the header's size val to include both blocks and consumed footer/header.
             *footer = *header;//make footer match
         }
         if (*(footer+1) % DSIZE == 0){//If next block is free
-            printf("b");
+            //printf("b");
             footer = footer + *(footer+1)/DSIZE + 2;//move footer to the footer of the next block
             *header += *footer + 2*DSIZE;
             *footer = *header;
@@ -69,9 +69,9 @@ size_t* coalesce(size_t* header){
 }
 
 size_t* extend_heap(size_t dwords){//helper function to extend heap.
-    //printf("%zx\n", dwords);
     if(dwords%2 == 1)
         dwords++;//align to 16 bytes
+    //printf("Extending the heap by %zx bytes.\n", dwords);
     size_t* next = (size_t*)mem_sbrk(dwords*DSIZE + 2*DSIZE);
     if(next == (size_t*)-1)
         return (size_t*)-1;
@@ -79,8 +79,12 @@ size_t* extend_heap(size_t dwords){//helper function to extend heap.
     //modify the last dwords + 3 entries in the heap. Set last one to epilogue, first one to header,
     //second-to-last to footer.
     *(next+dwords+1) = 1;//set last item to epilogue.
-    *(next-1) = dwords*DSIZE;//set previous epilogue to new header, not allocated.
+    *(next-1) = dwords*DSIZE;//set previous epilogue header to new header, not allocated.
     *(next+dwords) = *(next-1);//set new footer equal to new header.
+    int i;
+    for(i=0; i<dwords; i++){
+        *(next+i) = 0;
+    }
     return coalesce(next-1);
 }
 
@@ -89,14 +93,15 @@ size_t* extend_heap(size_t dwords){//helper function to extend heap.
  */
 int mm_init(void)
 {
-    heap = (size_t*)mem_sbrk(3*DSIZE);
+    heap = (size_t*)mem_sbrk(4*DSIZE);
     if(heap == (size_t*)-1)
         return -1;
-    heap[0] = 1;//Prologue header = 0 size, allocated
-    heap[1] = 1;//Prologue footer = 0 size, allocated
-    heap[2] = 1;//Epilogue header = 0 size, allocated
-    return 0;
+    heap[0] = 0;//First byte is 0
+    heap[1] = 1;//Prologue header = 0 size, allocated
+    heap[2] = 1;//Prologue footer = 0 size, allocated
+    heap[3] = 1;//Epilogue header = 0 size, allocated
     print_heap();
+    return 0;
 }
 
 /* 
@@ -105,9 +110,10 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    size = (size-1)/DSIZE + 1;//Change size to be the number of array entries we need.
-    if(size % 2 == 1)//Make sure it's 16-byte aligned.
-        size++;
+    //printf("Doing the malloc: %zx\n", size);
+    size_t asize = (size-1)/DSIZE + 1;//Calculate the number of array entries we need.
+    if(asize % 2 == 1)//Make sure it's 16-byte aligned.
+        asize++;
     size_t footer = 0;
     size_t header = 0;
     size_t header2 = 0;
@@ -118,28 +124,31 @@ void *mm_malloc(size_t size)
             if(heap[header] == size*DSIZE){//and exactly the right size
                 heap[header]++;
                 heap[footer]++;
-                printf("Whoa! Lucky!\n");
+                //printf("Whoa! Lucky!\n");
                 return (void*)(heap + header + 1);//return pointer to first block
             }
-            else if(heap[header] >= (size+4)*DSIZE){//or large enough
-                printf("Okay. Cool.\n");
-                header2 = header + size + 2;
+            else if(heap[header] >= (asize+4)*DSIZE){//or large enough
+                //printf("Okay. Cool.\n");
+                //print_heap();
+                //printf("%zx\n", heap[footer]);
+                header2 = header + asize + 2;
                 footer2 = footer;
-                footer = header + size + 1;//to eliminate conufusion, these are the locations of the headers and footers.
+                footer = header + asize + 1;//to eliminate conufusion, these are the locations of the headers and footers.
                 //line above was head + size/DSIZE s1 before
-                heap[header] = size*DSIZE + 1;//block 1 is size bytes, allocated
+                heap[header] = asize*DSIZE + 1;//block 1 is size bytes, allocated
                 heap[footer] = heap[header];//footer = header
-                heap[footer2] = heap[footer2] - (size+2)*DSIZE;//shrink second block by the size of block 1, and then make room for new header+footer
+                heap[footer2] = heap[footer2] - (asize+2)*DSIZE;//shrink second block by the size of block 1, and then make room for new header+footer
+                //printf("%zx\n", heap[footer2]);
                 heap[header2] = heap[footer2];//footer2 = header2
-                
+                //print_heap();
                 return (void*)(heap + header + 1);//return pointer to first block
                 //[80][ ][ ][ ][ ][ ][80] -> [33][ ][ ][33][16][ ][16], for example
             }
         }
         header = footer + 1;
     }
-    //printf("Extending heap\n");
-    size_t * loc = extend_heap(size);// this was at first * size_t but compiler didnt read that right
+    size_t * loc = extend_heap(asize);// this was at first * size_t but compiler didnt read that right
+    //printf("%zx\n", *(size_t*)0x80c29a8ed028);
     if(loc == (size_t*)-1){
         return (void*)-1;
     }
@@ -147,6 +156,7 @@ void *mm_malloc(size_t size)
         *loc = *loc + 1;//mark header as allocated
         *(loc + (*loc)/DSIZE +1) = *(loc + (*loc)/DSIZE +1) + 1;//mark footer as allocated
     }
+    //print_heap();
     return (void*)(loc + 1);
 }
 
@@ -155,16 +165,15 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
-    printf("%s\n", "FREE AT LAST\n");
+    //printf("%s\n", "FREE AT LAST\n");
     size_t* header = (size_t*)ptr - 1;
     size_t* footer = header + *header/8 + 1;
     if(*header %8 == 1){//if allocated
         *header = *header-1;//deallocate
         *footer = *footer-1;
     }
+    //print_heap();
     coalesce(header);
-
-    print_heap();
 }
 
 /*
